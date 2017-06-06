9cd65a2af397310a5261fb576fb1374527a3ce93
Bob-PC\Bob <bob.bumsuk.lee@gmail.com>
2017-06-06 16:00:22 +0900

    long press/touch to edit note
    - same look and feel on mobile/desktop

--- src/app/note/note.component.ts
+++ src/app/note/note.component.ts
@@ -3,11 +3,20 @@ import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 
 import { Note, Todo } from '../Note';
 import { NoteService } from '../service/note.service';
+// (click)="edit(note, $event)" onclick=""
 
 @Component({
   selector: 'note',
   template: `
-  <div *ngFor="let note of (noteService.notes | async)" (click)="edit(note)" class="notes">
+  <div *ngFor="let note of (noteService.notes | async)" 
+    (click)="click($event)" 
+    (mousedown)="start($event, note)" 
+    (touchstart)="start($event, note)" 
+    (mouseout)="cancel($event)" 
+    (touchend)="cancel($event)" 
+    (touchleave)="cancel($event)" 
+    (touchcancel)="cancel($event)" 
+    class="notes">
     {{note.name}}, {{note.updatedAt | date : 'dd/MM/yyyy h.mma' | lowercase}} <br/>
     {{note.text}}
     <hr>
@@ -16,6 +25,9 @@ import { NoteService } from '../service/note.service';
   styleUrls: ['./note.component.css']
 })
 export class NoteComponent implements OnInit {
+  longpress = false;
+  presstimer = null;
+  longtarget = null;
 
   constructor(private router: Router,
     public noteService: NoteService) { }
@@ -25,10 +37,47 @@ export class NoteComponent implements OnInit {
     this.noteService.todo = Todo.List;
   }
 
-  edit(note) {
-    console.log('edit', new Date(note.updatedAt));
+  private edit(note, e) {
+    console.log('edit', new Date(note.updatedAt), e);
 
     this.noteService.note = note;
     this.router.navigate(['group', this.noteService.groupName, 'edit', note.$key]);
+  }
+
+  // long press/touch, https://stackoverflow.com/a/27413909/588521
+  start(e, note) {
+    console.log(`start ${e.type}`);
+    if (e.type === "click" && e.button !== 0) return;
+    this.longpress = false;
+    //this.classList.add("longpress");
+    this.presstimer = setTimeout(() => {
+      //alert("long click");
+      this.edit(note, e);
+      this.longpress = true;
+    }, 750);
+
+    return false;
+  }
+
+  click(e) {
+    console.log(`click ${e.type}`);
+
+    if (this.presstimer !== null) {
+      clearTimeout(this.presstimer);
+      this.presstimer = null;
+    }
+    //this.classList.remove("longpress");
+    if (this.longpress) return false;
+    // alert("press");
+  }
+
+  cancel(e) {
+    console.log(`cancel ${e.type}`);
+
+    if (this.presstimer !== null) {
+      clearTimeout(this.presstimer);
+      this.presstimer = null;
+    }
+    //this.classList.remove("longpress");
   }
 }
