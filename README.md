d50280f43768fb7e7971f1a08633d611981b7f27
Bob-PC\Bob <bob.bumsuk.lee@gmail.com>
2018-08-22 17:49:56 +1200

    optional inputs for ng-idle-click
    - idleClickDebug to turn on debug log
    - idleClickLoader to override the default loader class name

--- README.md
+++ README.md
@@ -1,17 +1,16 @@
 # Our Notes
 
-This SPA(single-page app) allows to maintain notes by group. Each note can have an optional image taken from your chosen source - camera, storage or cloud. ~~This app supports PWA(progressive web app) using `@angular/service-worker` so you will enjoy native app-like look and feel, offline capability, better performance.~~
+This SPA(single-page app) allows to maintain notes by group. Each note can have an optional image from your camera, storage or cloud. ~~This app supports PWA(progressive web app) using `@angular/service-worker` so you will enjoy native app-like look and feel, offline capability, better performance.~~
 
 <img src="ScreenshotsOniPhone.PNG" width="60%">
 
 The project was built and deployed using following technologies:
 * [Angular](https://angular.io/): frontend framework
 * [Angular Firebase](https://github.com/angular/angularfire2): Angular library for Firebase
-* [Firebase](https://firebase.google.com/): cloud 
+* [Firebase](https://firebase.google.com/): cloud platform for hosting, authentication, database and storage
 * [Workbox](https://workboxjs.org/): JavaScript libraries for Progressive Web Apps
-* [Node.js](https://nodejs.org): JavaScript runtime environment
 
-Visit [the web](https://ng-notes-abb75.firebaseapp.com/) and play with it! You would need to log in first ~~as anonymous or~~ using your Facebook, Google account.
+Visit [the web](https://ng-notes-abb75.firebaseapp.com/) and play with it! You would need to log in first ~~as anonymous or~~ using your Facebook or Google account.
 
 For development, clone the repo and run `yarn` to install dependencies, then `ng serve`.
 
--- projects/ng-idle-click/src/lib/ng-idle-click.directive.ts
+++ projects/ng-idle-click/src/lib/ng-idle-click.directive.ts
@@ -1,38 +1,48 @@
-import { Directive, EventEmitter, HostBinding, 
-  HostListener, Input, Output  } from '@angular/core';
+import { Directive, ElementRef, EventEmitter, 
+  HostListener, Input, Output, Renderer2, OnInit  } from '@angular/core';
 import { NgIdleClickService } from './ng-idle-click.service';
 
+const defaultClassName = 'my-loader';
+
 @Directive({ selector: '[idleClick]' })
-export class NgIdleClickDirective {
-  @Input() debugLog: boolean = false;
+export class NgIdleClickDirective implements OnInit {
+  @Input() idleClickDebug: boolean = false;
+  @Input() idleClickLoader: string = defaultClassName;
   @Output() idleClick: EventEmitter<any> = new EventEmitter();
-  @HostBinding('class.my-loader') loading: boolean;
   @HostListener('click', ['$event'])
   onClick(e) {
     if (this.busyService.busy) {
-      if (this.debugLog) console.warn('busy');
+      if (this.idleClickDebug) console.warn('busy');
       return;
     } else {
-      if (this.debugLog) console.warn('click');
+      if (this.idleClickDebug) console.warn('click');
       this.busyService.set();
       this.timer = setTimeout(_ => { 
-        if (this.busyService.busy) 
-          this.loading = true; 
+        if (this.busyService.busy) {
+          this.renderer.addClass(this.el.nativeElement, this.idleClickLoader);
+        }
       }, 1000);
       this.idleClick.emit({ event: e, done: this.done });
     }
   }
   private timer;
 
-  constructor(private busyService: NgIdleClickService) { }
+  constructor(private busyService: NgIdleClickService, 
+    private el: ElementRef,
+    private renderer: Renderer2) { }
+
+  ngOnInit() {
+    if (this.idleClickDebug) console.log(`NgIdleClickDirective(${this.idleClickLoader})`);
+  }
 
   done = () => {
     this.busyService.reset();
-    this.loading = false;
+    this.renderer.removeClass(this.el.nativeElement, this.idleClickLoader);
+
     if (this.timer) {
       clearTimeout(this.timer);
       this.timer = null;
     }
-    if (this.debugLog) console.warn('done');
+    if (this.idleClickDebug) console.warn('done');
   };
 }
